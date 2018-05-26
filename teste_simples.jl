e814653ca3eeb15f72b496762d7d19b4a2f666ab
ThaizaRievrs <thaizarievrs@hotmail.com>
2018-05-25 21:23:07 -0300

    Update teste_simples.jl

--- teste_simples.jl
+++ teste_simples.jl
@@ -2,6 +2,7 @@ include("IRP.jl")
 
 H=[1,1,1,1,1]
 I0=[50000,5000,3000,2000,4000]
+r=[0, 0]
 Cap_estoque=[5000,3000,2000,4000]
 Demanda=[1000 0;
          3000 3000;
@@ -14,5 +15,5 @@ custo=[1 100 100 100 100;
        100 160 150 10 1]
 num_veiculos= 2
 Cap_veiculos=[5000,5000]
-num_periodos=1
-solveIRP(H,I0,Cap_estoque,Demanda,custo,num_veiculos,Cap_veiculos,num_periodos)
+num_periodos=2
+solveIRP(H,I0,r, Cap_estoque,Demanda,custo,num_veiculos,Cap_veiculos,num_periodos)
